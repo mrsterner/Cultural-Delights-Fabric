81f6d255e1a08abd56c6d7f9d739b2f8f69a4fce
mrsterner <simon@sterner.dev>
2023-01-12 19:10:38 +0100

    fix squid drop

--- src/main/java/dev/sterner/culturaldelights/CulturalDelights.java
+++ src/main/java/dev/sterner/culturaldelights/CulturalDelights.java
@@ -3,11 +3,17 @@ package dev.sterner.culturaldelights;
 import dev.sterner.culturaldelights.common.registry.CDObjects;
 import dev.sterner.culturaldelights.common.registry.CDWorldGenerators;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
@@ -17,6 +23,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Random;
 
 public class CulturalDelights implements ModInitializer {
+	private static final Identifier SQUID_LOOT_TABLE_ID = EntityType.SQUID.getLootTableId();
+	private static final Identifier GLOW_SQUID_LOOT_TABLE_ID = EntityType.GLOW_SQUID.getLootTableId();
 
 	@Override
 	public void onInitialize() {
@@ -28,6 +36,17 @@ public class CulturalDelights implements ModInitializer {
 			factories.add(new EmeraldToItemOffer(new ItemStack(CDObjects.EGGPLANT), 1, 10, 2, 0.2F));
 			factories.add(new EmeraldToItemOffer(new ItemStack(CDObjects.CORN_COB), 1, 10, 2, 0.2F));
 			factories.add(new EmeraldToItemOffer(new ItemStack(CDObjects.AVOCADO), 1, 10, 2, 0.2F));
+		});
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if(source.isBuiltin() && SQUID_LOOT_TABLE_ID.equals(id)){
+				LootPool.Builder poolBuilder = LootPool.builder().with(ItemEntry.builder(CDObjects.SQUID));
+				tableBuilder.pool(poolBuilder);
+			}
+			if(source.isBuiltin() && GLOW_SQUID_LOOT_TABLE_ID.equals(id)){
+				LootPool.Builder poolBuilder = LootPool.builder().with(ItemEntry.builder(CDObjects.GLOW_SQUID));
+				tableBuilder.pool(poolBuilder);
+			}
 		});
 	}
 
