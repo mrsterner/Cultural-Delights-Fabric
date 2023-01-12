3db7afb958feeecd58a06ea5459dfa1e2809a58c
mrsterner <simon@sterner.dev>
2023-01-12 03:00:40 +0100

    1.18

---
+++ src/main/java/dev/sterner/culturaldelights/common/registry/CDTags.java
@@ -0,0 +1,10 @@
+package dev.sterner.culturaldelights.common.registry;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class CDTags {
+    public static final TagKey<Item> CORN = TagKey.of(Registry.ITEM_KEY, new Identifier("c", "corn"));
+}
